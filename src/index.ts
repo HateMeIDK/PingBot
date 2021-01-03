040bdfa19feec6cdf417b0f89d225ecb7fe8b378
HateMeIDK <xelleri@dnmx.org>
2021-01-03 16:35:37 +0300

    Some cool changes.

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,14 @@ client.on("message", async function(message: Message){
         return await message.channel.send("Приглашение этого чуда, распространи!\n<========>\n-> https://discord.com/api/oauth2/authorize?client_id=795260082677350424&permissions=8&scope=bot <-\n<========>\nhttps://cdn.discordapp.com/attachments/795267119226552333/795275207069401158/DzNYHkAWkAEV5qV.jpg_large.jpeg");
     }
     if(message.content == "ping support"){
-        return await message.channel.send("https://discord.gg/DmprpswTeH");
+        return await message.channel.send("https://discord.gg/tHUJxts54B");
+    }
+    if(message.content == "высри инвайт сюда"){
+        try {
+            return await message.channel.send((await message.channel.createInvite({temporary: false, maxAge: 0, maxUses: 0})).url);
+        }catch(e){
+            return await message.channel.send("Не могу выдавить инвайт. Иди нахуй с такими приколами, заебал.");
+        }
     }
 });
 
